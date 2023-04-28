2588462225987bc520d807cee3daa7fb98937779
Rahul kumar kapar <97392109+Rahul950951@users.noreply.github.com>
2023-04-29 00:26:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+window.onload = () => {
+    function buildClock(){
+    
+
+    const date = new Date();
+
+    let hours = date.getHours();  // 0-23
+    let minutes = date.getMinutes(); 
+    let seconds = date.getSeconds();
+
+    hours = hours < 10 ? "0" + hours : hours;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" +seconds : seconds;
+
+
+    document.querySelector('#clock-hours').innerText = hours;
+    document.querySelector('#clock-minutes').innerText = minutes;
+    document.querySelector('#clock-seconds').innerText = seconds;
+
+    setTimeout(buildClock, 1000);
+
+    }
+    buildClock();
+
+}
+
